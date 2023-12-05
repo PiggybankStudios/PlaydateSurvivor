1a9019e04cacaffb8923f1f81d51e8291d540c12
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2023-12-04 20:32:55 -0800

    Added RunGame option to build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,14 @@
 @echo off
 
+set RunGame=1
+
 set PlaydateSdkDirectory=%PLAYDATE_SDK_PATH%
 set PdcExeName=%PlaydateSdkDirectory%\bin\pdc
 
 echo [Running PDC...]
 %PdcExeName% -sdkpath "%PlaydateSdkDirectory%" "..\src" "PlaydateSurvivor.pdx"
 echo [Done!]
+
+if "%RunGame%"=="1" (
+	%PlaydateSdkDirectory%\bin\PlaydateSimulator.exe "PlaydateSurvivor.pdx"
+)
